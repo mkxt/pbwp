b1f121098b06bd64d786688ca010c89ee2502162
mattiskuhn <74763697+mattiskuhn@users.noreply.github.com>
2022-02-01 18:08:07 +0100

    Update 06_split_text.py

--- 06_split_text.py
+++ 06_split_text.py
@@ -4,24 +4,32 @@ import sys
 import requests
 
 '''Create FPDF object.'''
+
 pdf = FPDF('P', 'mm', format='A5')
 pdf.set_margins(left=20, top=20, right=20)
 pdf.set_font('Courier')
 
+
 '''Render code.'''
-with open(os.getcwd()+'/'+__file__) as f:
+
+filename = sys.argv[0]
+with open(filename) as f:
     txt = f.read()
 
 pdf.add_page()
 pdf.set_font_size(10)
-pdf.multi_cell(148-40, 4, txt=txt, align='L')
+pdf.multi_cell(0, None, txt=txt)
+
 
 '''Request http content.'''
+
 url = 'https://loremipsum.de/downloads/original.txt'
 r = requests.get(url)
 txt = r.text
 
+
 '''Render text with increasing spaces.'''
+
 word_list = txt.split(' ')
 txt = ''
 for index, word in enumerate(word_list):
@@ -31,9 +39,10 @@ for index, word in enumerate(word_list):
 pdf.add_page()
 pdf.add_font('Vollkorn', '', 'fonts/Vollkorn-Regular.ttf', uni=True)
 pdf.set_font('Vollkorn')
-pdf.multi_cell(w=148-40, h=5, txt=txt, align='L')
+pdf.multi_cell(w=0, h=5, txt=txt, align='L')
+
 
 '''Save PDF.'''
-filename = sys.argv[0]
+
 filename = filename.replace('.py', '.pdf')
-pdf.output(filename)
+pdf.output('pdf/' + filename)
