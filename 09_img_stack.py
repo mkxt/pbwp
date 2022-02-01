9855a90e1839efcbf5ac4f7b370c8e9b38339efb
mattiskuhn <74763697+mattiskuhn@users.noreply.github.com>
2022-02-01 18:09:51 +0100

    Update 09_img_stack.py

--- 09_img_stack.py
+++ 09_img_stack.py
@@ -1,30 +1,35 @@
 from fpdf import FPDF
-import os
-import sys
-import requests
 import random
 from PIL import Image
+import glob
+import sys
 
 '''Create FPDF object.'''
+
 pdf = FPDF('P', 'mm', format='A5')
 pdf.set_margins(left=20, top=20, right=20)
 pdf.set_font('Courier')
 
+
 '''Render code.'''
-with open(os.getcwd()+'/'+__file__) as f:
+
+filename = sys.argv[0]
+with open(filename) as f:
     txt = f.read()
 
 pdf.add_page()
 pdf.set_font_size(10)
-pdf.multi_cell(148-40, 4, txt=txt, align='L')
+pdf.multi_cell(0, None, txt=txt)
+
 
 '''Add an image on top on each new page.'''
+
 pdf.set_margins(left=0, top=0, right=0)
 stack = []
-images = os.listdir(os.getcwd()+'/images')
+images = glob.glob('images/*')
 
-for image in images:
-    img_path = 'images/' + image
+for img_path in images:
+    # img_path = 'images/' + image
     img = Image.open(img_path)
     w, h = img.size
     size_factor = random.randint(1,5)/15
@@ -41,7 +46,8 @@ for i in range(len(stack)):
         pdf.image(d.get('img_path'), x=d.get('x'), y=d.get('y'),
             w=d.get('w'), h=d.get('h'))
 
+
 '''Save PDF.'''
-filename = sys.argv[0]
+
 filename = filename.replace('.py', '.pdf')
-pdf.output(filename)
+pdf.output('pdf/' + filename)
