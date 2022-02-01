9112d5ba95ff66e4c6292831f959d089cdee66c1
mattiskuhn <74763697+mattiskuhn@users.noreply.github.com>
2022-02-01 18:03:58 +0100

    Update 01_render_code.py

--- 01_render_code.py
+++ 01_render_code.py
@@ -1,21 +1,25 @@
 from fpdf import FPDF
-import os
 import sys
 
 '''Create FPDF object.'''
+
 pdf = FPDF('P', 'mm', format='A5')
 pdf.set_margins(left=20, top=30, right=20)
 pdf.set_font('Courier')
 
+
 '''Render code.'''
-with open(os.getcwd()+'/'+__file__) as f:
+
+filename = sys.argv[0]
+with open(filename) as f:
     txt = f.read()
 
 pdf.add_page()
 pdf.set_font_size(10)
-pdf.multi_cell(160, 4, txt=txt)
+pdf.multi_cell(0, None, txt=txt)
+
 
 '''Save PDF.'''
-filename = sys.argv[0]
+
 filename = filename.replace('.py', '.pdf')
-pdf.output(filename)
+pdf.output('pdf/' + filename)
